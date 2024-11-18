7efea2ec3872909af5d33cbdb309b2eea9794ca5
Coronado <justincoronado432@gmail.com>
2024-11-18 13:56:49 +0800

    Add files via upload

---
+++ assets/coronado.js
@@ -0,0 +1,8 @@
+const body = document.body
+
+const div = document.querySelector("div")
+const spanhi = document.querySelector("#hi")
+const spanbye = document.querySelector("#bye")
+
+spanhi.style.color = "red"
+
